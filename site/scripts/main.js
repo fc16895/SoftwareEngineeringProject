edb4945fa6561bda231abab940a3695d4bb72f0d
CodingKaNi <74120806+CodingKaNi@users.noreply.github.com>
2021-04-10 02:31:59 +0800

    just for learning

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -1,2 +1,18 @@
-let myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+let myHeading=document.querySelector('h1');
+let myButton=document.querySelector('button')
+function setUserName(){
+    let usrName=prompt("My name is___");
+    if(!usrName || usrName===null)
+        setUserName();
+    localStorage.setItem('name',usrName);
+    myHeading.textContent= '欢迎 '+usrName+'!';
+}
+if(!localStorage.getItem('name')){
+    setUserName();
+}
+else{
+    myHeading.textContent='欢迎 '+localStorage.getItem('name')+'!';
+}
+myButton.onclick=function(){
+    setUserName();
+}
