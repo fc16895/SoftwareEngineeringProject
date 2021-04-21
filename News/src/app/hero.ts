bc91da3c6b93b34dd5e858b14beaecb69dbf53ef
Zoetinker <470828762@qq.com>
2021-04-21 11:28:27 +0100

    first

---
+++ News/src/app/hero.ts
@@ -0,0 +1,4 @@
+export interface Hero {
+  Title: string;
+  Rate: string;
+}
---
+++ News/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { Title: 'Depending on whether you are using a CSS preprocessor, the extension on your CSS files can vary.', Rate: '10' },
+  { Title: 'Depending on whether you are using a CSS preprocessor, the extension on your CSS files can vary.', Rate: '10' },
+  { Title: 'Depending on whether you are using a CSS preprocessor, the extension on your CSS files can vary.', Rate: '10' },
+  { Title: 'Depending on whether you are using a CSS preprocessor, the extension on your CSS files can vary.', Rate: '10' },
+  { Title: 'Depending on whether you are using a CSS preprocessor, the extension on your CSS files can vary.', Rate: '10' },
+  { Title: 'Depending on whether you are using a CSS preprocessor, the extension on your CSS files can vary.', Rate: '10' },
+  { Title: 'Depending on whether you are using a CSS preprocessor, the extension on your CSS files can vary.', Rate: '10' },
+  { Title: 'Depending on whether you are using a CSS preprocessor, the extension on your CSS files can vary.', Rate: '10' },
+  { Title: 'Depending on whether you are using a CSS preprocessor, the extension on your CSS files can vary.', Rate: '10' }
+];
+
