3d398af7ab1807b0c1d73ad15108702c271a8d43
Felipe <fc16895@my.bristol.ac.uk>
2021-04-29 20:44:27 +0100

    Chess boards pics added

--- neededfiles/src/app/boards/boards.component.ts
+++ neededfiles/src/app/boards/boards.component.ts
@@ -14,14 +14,23 @@ import {DashboardComponent } from '../dashboard/dashboard.component'
 
 export class BoardsComponent implements OnInit {
 
-	ImagePath: String;
+	imagePath: String;
 
 	links: Link[]=[];
+
+	average = 0;
   
   constructor( private outputService: OutputService) {
-	  this.ImagePath = '/assets/sample.png';
-  }
-    //image location
+
+
+
+
+
+	  this.imagePath = '/assets/StartPhase.png';
+  
+
+
+}
 	
 	
 	
@@ -29,14 +38,69 @@ export class BoardsComponent implements OnInit {
 /* 	  console.log('put');
   	this.putLinks(); */
 	this.getLinks();
-  }
+ }
+
+
+
 getLinks(): void {
 	this.outputService.getLinks(DashboardComponent.toSearch)
 		.subscribe(links => {
 			this.links=links;
 			console.log(this.links);
-		});
+			console.log(this.links.length);	
+	
+/*Here below we calculate the average rating (this wasnt done on a separate functtion because for some
+ reason the links were not initiliased when using a separate function !) */
+
+	this.average = 0;
+        for (var i = 0; i < this.links.length;i++ ){
+                this.average = this.average + this.links[i].rating;
+                console.log(this.links[i].rating);
+        }
+        this.average = this.average/this.links.length;
+        console.log(this.average);
+
+
+if (this.average > 0 && this.average <=2){
+
+this.imagePath = '/assets/FirstPhase.png';
+
 }
+
+if (this.average > 2 && this.average <=4.5){
+
+this.imagePath = '/assets/SecondPhase.png';
+
+}
+
+if (this.average > 4.5 && this.average <=6.5){
+
+this.imagePath = '/assets/ThirdPhase.png';
+
+}
+
+if (this.average > 6.5 && this.average <=8){
+
+this.imagePath = '/assets/FourthPhase.png';
+
+}
+
+
+if (this.average > 8 && this.average <=9){
+
+this.imagePath = '/assets/FifthPhase.png';
+
+}
+
+if (this.average > 9 && this.average <=10){
+
+this.imagePath = '/assets/FinalPhase.png';
+
+}
+	});
+}
+
+
 
 
 //ratingService.getRatings().subscribe(rating => this.myRatings = rating);
