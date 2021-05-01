8ff47e4f98e8dca6aef785205fbf42e4b2a6c99d
Linhong Dai <linhongdai@LinhongdeMacBook-Pro.local>
2021-05-01 16:50:40 +0100

    rating page updated

--- neededfiles/dist/CauseVisualiser/polyfills.js
+++ neededfiles/dist/CauseVisualiser/polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/fzcastan/diffImagesVersion2/SoftwareEngineeringProject/neededfiles/src/polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! /Users/linhongdai/Desktop/sw/SoftwareEngineeringProject/neededfiles/src/polyfills.ts */"hN/g");
 
 
 /***/ }),
